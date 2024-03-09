0c6e80ff0dea67d434b128f453a2b676a15a5164
Rickard <rickard_b@live.com>
2024-03-09 14:41:41 +0700

    signin completed

---
+++ MyWebApplication/Models/Views/SignInViewModel.cs
@@ -0,0 +1,11 @@
+﻿using MyWebApplication.Models.Forms;
+
+namespace MyWebApplication.Models.Views;
+
+public class SignInViewModel
+{
+
+	public SignInFormModel Form { get; set; } = new SignInFormModel();
+
+	public string? ErrorMessage { get; set; }
+}
