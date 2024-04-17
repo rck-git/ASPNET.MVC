8aa33745289af95f0e8eb46c4253bf7106803d2e
Rickard <rickard_b@live.com>
2024-04-17 14:20:15 +0200

    Courses init

---
+++ MyWebApplication/Controllers/CoursesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static System.Net.WebRequestMethods;
+
+namespace MyWebApplication.Controllers;
+
+public class CoursesController : Controller
+{
+	public IActionResult Index()
+	{
+		HttpClient client = new HttpClient();
+		client.GetAsync("https://localhost:7040/api/Course");
+		return View();
+	}
+}
